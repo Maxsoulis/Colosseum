cdaa895bbc18f527ae517578d33a900fc5d008bf
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-05 15:18:29 -0500

    Create Item.h

---
+++ Item.h
@@ -0,0 +1,14 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <string>
+#include <iostream>
+using std::string;
+using std::cout;
+class Item{
+  public:
+  string name;
+  int attack, health, defense;
+  Item(string = " ", int = 0, int = 0, int = 0);
+  void printStats();
+};
+#endif
