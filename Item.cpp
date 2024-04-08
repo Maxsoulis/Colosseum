6d104a99f36d114c6d0e88ab3385009ad75f56f4
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-07 22:44:11 -0500

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,11 +1,13 @@
 #include "Item.h"
-Item::Item(string n, int h, int a, int d ){
+Item::Item(string n, int h, int a, int d, int p,int mDA  ){
   name = n;
   health = h;
   attack = a;
   defense = d;
+  price = p;
+  maxDropAmount = mDA;
 }
-Item::printStats(){
+void Item::printStats(){
   cout << name << '\n';
   cout << "Increases health by " << health << " points" <<'\n';
   cout << "Increases attack by " << attack << " points" << '\n';
