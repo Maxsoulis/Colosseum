b0ef10140e43a069ab34720808e4ba58f0b299d7
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-08 21:58:20 -0500

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,18 +1,19 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 #include "Item.h"
+#include "GameCharacter.h"
 #include <vector>
  
 using std::vector;
-class Enemy{
-  public:
+class Enemy: public GameCharacter{
+  private:
   Item enemyDrop;
   string name;
-  int health, attack, defense;
+  int health, attack, defense, maxDropAmount, amountDropped;
   vector<Item> enemyDrops;
-  Enemy(string = " ", int = 0, int = 0, int = 0, vector<Item> = vector<Item>());
+  public:
+  Enemy(string = " ", int = 0, int = 0, int = 0,int = 0, vector<Item> = vector<Item>());
   void pickDrop();
-
-
+  void printDrop();
 };
 #endif
