7d2eaabded7adeb4016a4f4097b4500ccc048618
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-08 21:57:59 -0500

    Update GameCharacter.h

--- GameCharacter.h
+++ GameCharacter.h
@@ -3,10 +3,21 @@
 #include <string>
 using std::string;
 class GameCharacter{
-  public:
+  private:
   string name;
   int currentHealth, maxHealth, attack, defense;
+  public:
   GameCharacter(string, int, int, int);
+  string getName();
+  int getHealth();
+  int getAttack();
+  int getCurrentHealth();
+  int getMaxHealth();
+  int getDefense();
+  void setNewName(string);
+  void setNewAttack(int);
+  void setNewHealth(int);
+  void setNewDefense(int);
   int takeDamage(int);
   bool checkIsDead();
 };
