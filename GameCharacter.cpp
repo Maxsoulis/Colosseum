a069ebe493e34969704c829941b6805ff50d00bd
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-08 21:56:23 -0500

    Update GameCharacter.cpp

--- GameCharacter.cpp
+++ GameCharacter.cpp
@@ -7,6 +7,34 @@ GameCharacter::GameCharacter(string n, int h, int a, int d ) {
     attack = a;
     defense = d;
 }
+string GameCharacter::getName(){
+  return name;
+}
+int GameCharacter::getAttack(){
+  return attack;
+}
+int GameCharacter::getCurrentHealth(){
+  return currentHealth;
+}
+int GameCharacter::getMaxHealth(){
+  return maxHealth;
+}
+int GameCharacter::getDefense(){
+  return defense;
+}
+void GameCharacter::setNewName(string newName){
+  name = newName;
+}
+void GameCharacter::setNewAttack(int newAttack){
+  attack = newAttack;
+}
+void GameCharacter::setNewHealth(int newHealth){
+  currentHealth = newHealth;
+  maxHealth = newHealth;
+}
+void GameCharacter::setNewDefense(int newDefense){
+  defense = newDefense;
+}
 int GameCharacter::takeDamage(int amount){
   int damage = amount;
   currentHealth -= damage;
