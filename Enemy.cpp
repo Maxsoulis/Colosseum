f6cc59654f5b64d27be3f0528e98648472450fe8
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-09 11:08:36 -0500

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,9 +1,6 @@
-#include "Enemy.h"
-Enemy::Enemy(string n, int h,int a, int d, vector<Item> eDrops)  {
-  name = n;
-  health = h;
-  attack = a;
-  defense = d;
+#include "Enemy.h "
+Enemy::Enemy(string n, int h,int a, int d,int mDA, vector<Item> eDrops): GameCharacter(n, h, a, d){
+  maxDropAmount = mDA;
   enemyDrops = eDrops;
 }
 void Enemy::pickDrop(){
@@ -11,10 +8,14 @@ void Enemy::pickDrop(){
   srand((unsigned) time(NULL));
   int itemChooser = rand() % size;
   auto it = enemyDrops.begin();
+  cout << itemChooser << '\n';
   for(int i = 0; i < itemChooser; i++){
     it++;
   }
   enemyDrop = *it;
-  int amountDropped = rand() % enemyDrop.maxDropAmount;
+  amountDropped = rand() % maxDropAmount;
   
+}
+void Enemy::printDrop(){
+  cout << "The " << getName() << " " << "dropped" << " "0 << amountDropped << " " << enemyDrop.name<<'\n';
 }
