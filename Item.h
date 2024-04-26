e1d08431cdda79a970ef29405876958b190a026d
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-25 21:00:22 -0500

    Update Item.h

--- Item.h
+++ Item.h
@@ -2,14 +2,19 @@
 #define ITEM_H
 #include <string>
 #include <iostream>
+#include "GameCharacter.hpp"
 using std::string;
 using std::cout;
-class Item{
-  public:
+class Item : public GameCharacter{
+  private:
   string name;
-  int attack, health, defense, price, maxDropAmount;
-  Item(string = " ", int = 0, int = 0, int = 0, int = 0, int = 0);
+  int attack, health, defense, price, quantity;
+  public:
+  Item(string = " ", int = 0, int = 0, int = 0, int = 0, int = 0 );
   void printStats();
+  int getItemPrice();
+  void setNewItemPrice(int);
+  int getQuantity();
+  void updateQuantity( int);
 };
 #endif
-
