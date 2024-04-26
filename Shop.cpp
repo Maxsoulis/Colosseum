7d0e9289cbd98c82d2a4698797f5a9def4313b40
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-04-25 21:56:52 -0500

    Update Shop.cpp

--- Shop.cpp
+++ Shop.cpp
@@ -1,22 +1,35 @@
-#include "Shop.h"
+#include "Shop.hpp"
 
-Shop::Shop(vector<Item> i){
-  items = i;
+Shop::Shop(vector<Item> sI, vector<Item> fI, vector<Item> p, vector<Item> w, vector<Item> a){
+  specialItems = sI;
+  foodItems = fI;
+  potions = p;
+  weapons = w;
+  armor = a;
 }
 void Shop::introduceShop(){
   cout << "Welcome to the shop, where you can buy items and equipment to use in battle and sell items that you get." <<'\n';
 }
 string Shop::shopDialogue(){
+  //creates dialogue options and will randomly pick one when opening the shop.
     dialogueOptions[0] = "Hey, back again huh.";
     dialogueOptions[1] = "How are we today?";
-    dialogueOptions[2] = "Me and Marcell are hitting the bar later care to join? Nah yer too soft";
+    dialogueOptions[2] = "Me and Marcell are hitting the bar later care to join?  ";
     dialogueOptions[3] = "Battles are hard fought with brave men, strong weapons, and some nice potions";
 }
-void Shop::presentItems(vector<Item> items){
+void Shop::presentItems(vector<Item> items ){
+  //iterates through a vector of items and prints all of them
+  string alphabet = "abcdefghijklmnopqrstuvwxyz";
+  char letter;
+  int alphabetPos = 0;
   int size  = items.size();
   auto it = items.begin();
   while(it != items.end()){
-    cout << &it;
+    item = *it;
+    letter = alphabet[alphabetPos];
+    cout << letter<< ". " << item.getName() <<'\n';
     it++;
+    alphabetPos++;
   }
+
 }
