aeb12da1483e795de2fcd3d724ed3e030701953c
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-05-03 14:31:00 -0500

    Update Empire.h

--- Empire.h
+++ Empire.h
@@ -2,16 +2,20 @@
 #define EMPIRE_H
 #include "Player.h"
 #include "Colosseum.h"
+using std::cin;
 class Empire{
 private:
+Item dummyItem;
 public:
+Player player;
+Empire(Player);
 vector<Item> item;
 int performEndGameLogic();
 int runEmpire();
-void handleColosseumActions(Colosseum*);
-void handleBattleActions(Enemy *);
+void handleColosseumActions(Colosseum);
+void handleBattleActions(Enemy);
 void handleBuyingActions( Shop, vector<Item>);
-void handleShopActions(Shop*);
+void handleShopActions(Shop);
 void handleMovementActions( );
 void printActions(int, string[]);
 };
