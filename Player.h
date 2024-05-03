77aee5b94dd822a762af1ddc1eb7fd01e031d5ac
Maxsoulis <156605163+Maxsoulis@users.noreply.github.com>
2024-05-03 14:32:40 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,20 +1,23 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-#include "Shop.h"
-#include "GameCharacter.h"
+#include "Shop.hpp"
+#include "GameCharacter.hpp"
+using std::cin;
 class Player : public GameCharacter{
  private:
  string name;
- int health, attack, defense, money;
- Item copyOfItem;
+ int health, attack, defense, money, itemPosition;
+ Item copyOfItem, dummyItem;
  public:
+ Item currentItem;
  vector<Item> inventory;
  Player(string = "", int = 0, int = 0, int = 0, int = 0,  vector<Item> = vector<Item>() );
  void removeItem(Item &);
  void addItem(Item&);
  void buyItem(int, Item&);
- void sellItem(int, Item&);
+ void sellItem(Item&);
  int getMoney();
  void setNewMoney(int);
+ bool findItem(Item, string);
 };
 #endif
